14da16f8189e0ac6fd3af41b968f2fa25e2ffbd9
dyhas <example@gmail.com>
2021-03-04 18:38:38 +0500

    first comit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// // answers[0] = prompt("Как ваше имя?", "");
+// // answers[1] = prompt("Сколько вам лет?", "");
+// // answers[2] = prompt("Ваш любимый цвет?", "");
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+// console.log(`http://someurl.com/${category}/5`);
+
+// const user = prompt("Как ваше имя?");
+// alert(`Привет, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
